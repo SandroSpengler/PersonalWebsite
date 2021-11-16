13e56fb539d8f2051c71174c1569c17680920747
Sandro <sandro.spengler@web.de>
2021-11-16 02:09:57 +0100

    Basic Components and Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="*" element={<HomePage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="home" element={<HomePage />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = (props: any) => {
+  return (
+    <div>
+      <p>Home Page</p>
+    </div>
+  );
+};
+
+export default HomePage;
